969a4823e533bd3f1e9a1b03828ccc0c52f88b29
Dario Malchiodi <malchiodi@di.unimi.it>
2012-10-10 15:07:05 +0200

    Added documentation.

--- neosAPI.m
+++ neosAPI.m
@@ -13,43 +13,43 @@
 
 BeginPackage["neosAPI`"]
 
-neosHelp::usage = "neosHelp[] returns a general help message";\
+neosHelp::usage = "neosHelp[] returns a generic help message.";\
 
-neosEmailHelp::usage = "neosEmailHelp[] returns a general help message for email users";\
+neosEmailHelp::usage = "neosEmailHelp[] returns a generic help message for email users.";\
 
-neosWelcome::usage = "neosWelcome[] returns a welcome message";\
+neosWelcome::usage = "neosWelcome[] returns a welcome message.";\
 
-neosVersion::usage = "neosVersion[] returns a string containing the  version number of the NEOS server";\
+neosVersion::usage = "neosVersion[] returns a string containing the  version number of the NEOS server.";\
 
-neosPing::usage = "neosPing[] returns the string \"NeosServer is alive\" when the NEOS server is running";\
+neosPing::usage = "neosPing[] returns the string \"NeosServer is alive\" when the NEOS server is running.";\
 
-neosPrintQueue::usage = "neosPrintQueue[] returns a string containing the current NEOS jobs";\
+neosPrintQueue::usage = "neosPrintQueue[] returns a string containing the current NEOS jobs.";\
 
-neosGetSolverTemplate::usage = "neosGetSolverTemplate[category, solverName, inputMethod] returns a template for a given solver";\
+neosGetSolverTemplate::usage = "neosGetSolverTemplate[category, solverName, inputMethod] returns a template for the solver identified by category, solverName and inputMethod.";\
 
-neosListAllSolvers::usage = "neosListAllSolvers[] returns a string listing all solvers available on NEOS, each shown in the format category:solver:inputMethod";\
+neosListAllSolvers::usage = "neosListAllSolvers[] returns a string listing all solvers available on NEOS, each shown in the format category:solver:inputMethod.";\
 
-neosListCategories::usage = "neosListCategories[] returns a dictionary containing all categories available on NEOS";\
+neosListCategories::usage = "neosListCategories[] returns a dictionary containing all categories available on NEOS.";\
 
-neosListSolversInCategory::usage = "neosListSolversInCategory[category] returns a string listing all solvers for a given category, each shown in the format solver:input";\
+neosListSolversInCategory::usage = "neosListSolversInCategory[category] returns a string listing all solvers for the category category, each shown in the format solver:inputMethod.";\
 
-neosSubmitJob::usage = "neosSubmitJob[request] submits tho the NEOS server a given catetory encoded in standard XML-RPC format, returning a list {jobNumber, password} or a string containing the description of an occurred error}";\
+neosSubmitJob::usage = "neosSubmitJob[request] submits a job tho the NEOS server, encoded in request using the standard XML-RPC format for a given catetory, returning a list {jobNumber, password} or a string containing the description of an occurred error.";\
 
-neosGetJobStatus::usage = "neosGetJobStatus[jobNumber, password] returns either \"Done\", \"Running\", \"Waiting\", \"Unknown Job\", or \"Bad Password\" according to the given job status";\
+neosGetJobStatus::usage = "neosGetJobStatus[jobNumber, password] returns either \"Done\", \"Running\", \"Waiting\", \"Unknown Job\", or \"Bad Password\" according to the status of the job identified by jobNumber and password.";\
 
-neosGetFinalResults::usage = "neosGetFinalResults[jonNumner, password] rerturns a string containing the given job's output. If the job is still running, the function will hang until it completes.";\
+neosGetFinalResults::usage = "neosGetFinalResults[jonNumner, password] returns a string containing the output of the job specified by jobNumber and password. If the job is still running, the function will hang until it completes.";\
 
-neosGetJobInfo::usage = "neosGetJobInfo[jobNumber, password] returns a string describing a 4-ple (category, solverName, input, status) describing the specified job status";\
+neosGetJobInfo::usage = "neosGetJobInfo[jobNumber, password] returns a string describing a 4-ple (category, solverName, input, status) describing the status of the job identified by jobNumber and password.";\
 
-neosKillJob::usage = "neosKillJob[jobNumber, password] kills the specified job.";\
+neosKillJob::usage = "neosKillJob[jobNumber, password] kills the job identified by jobNumber and password.";\
 
-neosGetFinalResultsNonBlocking::usage = "neosGetFinalResultsNonBlocking[jobNumber, password] retrieves the results of a job, returning an empty string if the latter is still running. In this way the function always returns without hanging.";\
+neosGetFinalResultsNonBlocking::usage = "neosGetFinalResultsNonBlocking[jobNumber, password] retrieves the results of the job identified by jobNumber and password, returning an empty string if the latter is still running. In this way the function always returns without hanging.";\
 
-neosGetIntermediateResults::usage = "neosGetIntermediateResults[jobNumber, password, offset] retrieves intermediate results of a job, starting at character offset up to the last received data. The function hangs until another packet of output is sent or the job is finished.";\
+neosGetIntermediateResults::usage = "neosGetIntermediateResults[jobNumber, password, offset] retrieves intermediate results of the job identified by jobNumber and password, starting at character offset up to the last received data. The function hangs until another packet of output is sent or the job is finished.";\
 
-neosGetIntermediateResultsNonBlocking::usage = "neosGetIntermediateResultsNonBlocking[jobNumber, password, offset] retrieves intermediate results of a job, starting at character offset up to the last received data. The function returns an empty string if the job is still running.";\
+neosGetIntermediateResultsNonBlocking::usage = "neosGetIntermediateResultsNonBlocking[jobNumber, password, offset] retrieves intermediate results of the job identified by jobNumber and password, starting at character offset up to the last received data. The function returns an empty string if the job is still running.";\
 
-neosSolveJob::usage = "neosSolveJon[request] submits tho the NEOS server a given catetory encoded in standard XML-RPC format and returns the corresponding job output. The function will hang until the job completes.";\
+neosSolveJob::usage = "neosSolveJob[request] submits a job tho the NEOS server, encoded in the standard XML-RPC format for a given catetory, and returns the corresponding job output. The function will hang until the job completes.";\
 
 Begin["`Private`"]
 
